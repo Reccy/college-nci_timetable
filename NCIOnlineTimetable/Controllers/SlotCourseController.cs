bc5955b56d24d0c0b900a53fe066867f5e44b80c
Reccy <aaronmeaney@hotmail.com>
2016-11-12 22:23:56 +0000

    Added course details to side pane.

---
+++ NCIOnlineTimetable/Controllers/SlotCourseController.cs
@@ -0,0 +1,25 @@
+﻿using NCIOnlineTimetable.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace NCIOnlineTimetable.Controllers
+{
+    public class SlotCourseController : System.Web.Http.ApiController
+    {
+        [ResponseType(typeof(string))]
+        public IHttpActionResult GET(string slotId)
+        {
+            TimetableContext context = new TimetableContext();
+
+            List<int> slot = context.Database.SqlQuery<int>(@"SELECT Timetable_Id FROM Slots WHERE Id = '" + slotId + "';").ToList<int>();
+            System.Diagnostics.Debug.WriteLine("Id: " + slot[0]);
+            List<string> course = context.Database.SqlQuery<string>(@"SELECT Name FROM Timetables WHERE Id = '" + slot[0] + "';").ToList<string>();
+
+            return Ok(course[0]);
+        }
+    }
+}
